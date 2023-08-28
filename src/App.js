4a18166fb122f5853a131c11d7558dbad58d7574
olymoodbughnes <yairomanuel@gmail.com>
2023-08-28 16:42:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+
+        <div className="main-content">
+          <div className='navbar'>
+
+          </div>
+          <div className='game-content'>two</div>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
